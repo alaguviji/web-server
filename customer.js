e7a47f07766c1c5fed7aac85008e8419e978922c
Alagarasan <vijay.alagarasan@RTCITSMACRFFH05.local>
2016-09-15 06:49:00 -0500

    Initial Repo

---
+++ customer.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000);
+
+app.get('/customers', function(req,res){
+  customer ={
+    firstname: 'Vijay',
+    lastname: 'Alagarasan',
+    emailaddress: 'Vijay.Alagarasan@asurion.com'
+  }
+  res.send(JSON.stringify(customer));
+});
+
+app.get('/accounts', function(req,res){
+  account ={
+    name: 'Facebook',
+    username: 'alaguviji@gmail.com'
+  }
+
+  res.send(JSON.stringify(account));
+
+});
+
+
+
+console.log("Webserver Started");
---
+++ home.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express ();
+
+var middleware = {
+  requireAuthentication: function(req, res, next){
+    console.log('Successfully Authenticated');
+    next();
+  },
+  logger: function(req, res, next){
+    console.log(req.method +" "+req.originalUrl+ " "+req.body);
+    next();
+  }
+};
+
+
+app.listen(3000);
+
+app.use(middleware.logger);
+app.use(express.static(__dirname+'/public'));
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+  res.send('About us!!');
+})
